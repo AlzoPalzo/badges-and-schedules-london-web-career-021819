c1e55b7cdb01ec852a864f3c92fcf0cc890b63a4
Ali Madni <ali555m@hotmail.com>
2019-02-10 15:19:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,11 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
-
+  rooms = []
+  speakers.each_with_index do |speaker, index|
+    rooms.push("Hello, #{speaker} You'll be assigned to room #{index + 1}!")
+    
+  }
 end
 
 def printer()
