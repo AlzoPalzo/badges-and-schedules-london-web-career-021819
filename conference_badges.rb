3caa7fb9e0cf31c20b3dbc679b048af9c6bf9ad2
Ali Madni <ali555m@hotmail.com>
2019-02-10 15:19:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(speakers)
   rooms = []
   speakers.each_with_index do |speaker, index|
     rooms.push("Hello, #{speaker} You'll be assigned to room #{index + 1}!")
-
+  end
   }
 end
 
