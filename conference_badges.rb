5640d8cb0c00ae43508601b15b7dbe11f83e06df
Ali Madni <ali555m@hotmail.com>
2019-02-10 15:01:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,18 @@
 def badge_maker(name)
   "Hello my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |message|
+    badge_array.push(badge_maker(message))
+  
+end
+
+def assign_rooms(speakers)
+
+end
+
+def printer()
+
+end
